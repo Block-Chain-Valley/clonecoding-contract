3548e8027794448fd8a1fa8f738c18cfeb7521eb
cwrong <sjy0175@gmail.com>
2022-10-08 19:38:05 +0900

    I hate Pororo

---
+++ contracts/MyToken.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "./myERC20.sol";
+
+contract MyToken is myERC20 {
+  uint public INITIAL_SUPPLY = 10000000000000000000000000000;
+  constructor() public myERC20("cWrong Token", "CWT") {
+    _mint(msg.sender, INITIAL_SUPPLY);
+  }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,36 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const MyToken = await hre.ethers.getContractFactory("MyToken");
+  const myToken = await MyToken.deploy();
+
+  await myToken.deployed();
+
+  console.log("Your deployed contract address:", myToken.address);
+
+  fs.writeFile('./deployedAddress', myToken.address, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  })
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
