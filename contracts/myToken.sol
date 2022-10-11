a9676c332f4c6502b31bc65a0d45ec7a29450a9e
JangHyuk00 <yim0823@hotmail.com>
2022-10-11 22:27:37 +0900

    any message

---
+++ contracts/myToken.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "./myERC20.sol";
+
+contract MyToken is myERC20 {
+  uint public INITIAL_SUPPLY = 1e18;
+  constructor() public myERC20("janghyuk", "JHT") {
+    _mint(msg.sender, INITIAL_SUPPLY);
+  }
+}
